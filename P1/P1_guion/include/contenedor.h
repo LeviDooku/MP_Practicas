1a48213cfc96e268ab2470029c43da3af73b572c
LeviDooku <pedrovelascosantana@gmail.com>
2025-02-21 12:24:01 +0100

    empezando ficheros cabecera

--- P1/P1_guion/include/contenedor.h
+++ P1/P1_guion/include/contenedor.h
@@ -4,8 +4,19 @@
 // Pedro Velasco Santana      //
 ////////////////////////////////
 
+// TO DO: mirar parámetros: valor, referencia, referencia constante?
+
 #ifndef CONTENEDOR_H
 #define CONTENEDOR_H
 
+struct contenedor;
+
+void agregaPaquete(contenedor c, paquete p);
+void mostrarContenedor(const contenedor c); //Evitar, preferible string o algo del estilo 
+int calcularPeso(const contenedor c); //devolver en kg!!! Está en gramos
+contenedor obtenienePaquetes(const contenedor c, const int origen); //Comprobar que el valor devuelto es correcto
+void ordenar(contenedor c);
+? enviosDestino(const contenedor c, const int destino); //Devuelve un vector? wtff
+void analisisDestinos(const contenedor c); //Not sure lo q devuelve
 
 #endif
--- P1/P1_guion/include/paquete.h
+++ P1/P1_guion/include/paquete.h
@@ -4,8 +4,15 @@
 // Pedro Velasco Santana      //
 ////////////////////////////////
 
+// TO DO: mirar parámetros: valor, referencia, referencia constante?
+
 #ifndef PAQUETE_H
 #define PAQUETE_H
 
+struct paquete;
+
+String toString(const paquete p); //Por q da error el string. Mirar formato en pdf
+paquete creaPaquete(const int origen, const int destino, const int peso);
+bool esValido(const paquete p);
 
 #endif
