e25c8bc2ffc2f4352147d6029f4357bc66853197
LeviDooku <pedrovelascosantana@gmail.com>
2025-02-25 20:05:34 +0100

    Falta comentar en Doxygen

--- P1/P1_guion/include/contenedor.h
+++ P1/P1_guion/include/contenedor.h
@@ -9,20 +9,76 @@
 
 #include "paquete.h"
 
-struct Contenedor{
-    static const int MAX = 100;
-    Paquete lista[MAX];
-    int utiles = 0;
+/**
+ * @struct Contenedor
+ * @brief Contenedor de paquetes con capacidad máxima fija.
+ */
+struct Contenedor {
+    static const int MAX = 100;   /**< Número máximo de paquetes permitidos */
+    Paquete lista[MAX];           /**< Lista de paquetes almacenados */
+    int utiles = 0;               /**< Número de paquetes actualmente almacenados */
 };
 
+/**
+ * @brief Agrega un paquete al contenedor si es válido.
+ * @param c Referencia al contenedor donde se agregará el paquete.
+ * @param p Referencia al paquete a agregar.
+ */
 void agregaPaquete(Contenedor &c, Paquete &p);
+
+/**
+ * @brief Muestra la información de todos los paquetes del contenedor.
+ * @param c Contenedor del que se mostrarán los paquetes.
+ */
 void mostrarContenedor(const Contenedor &c);
+
+/**
+ * @brief Calcula el peso total de los paquetes del contenedor en kilogramos.
+ * @param c Contenedor del que se calculará el peso.
+ * @return Peso total en kilogramos.
+ */
 float calcularPeso(const Contenedor &c);
+
+/**
+ * @brief Construye un contenedor nuevo con los paquetes que provengan de origen
+ * @param c Contenedor a analizar.
+ * @param origen Origen
+ * @return Contenedor nuevo
+ */
 Contenedor obtienePaquetes(const Contenedor &c, const int origen);
+
+/**
+ * @brief Intercambia los paquetes a y b.
+ * @param a Paquete a.
+ * @param b Paquete b
+ */
 void intercambiaPaquetes(Paquete &a, Paquete &b);
+
+/**
+ * @brief Ordenación personalizada, basada en el método de la burbuja.
+ * @param c Contenedor a ordenar.
+ */
 void bubbleSort_pers(Contenedor &c);
+
+/**
+ * @brief Ordena un contenedor teniendo en cuenta el origen (primero) y destino (segundo)
+ * @param c Contenedor a ordenar.
+ */
 void ordenar(Contenedor &c);
+
+/**
+ * @brief Obtiene el número de paquetes de un contenedor c que van a un destino, además del total de su peso en kg.
+ * @param c Contenedor a analizar.
+ * @param destino Destino criterio
+ * @param nro Contendrá el número de paquetes de destino <destino>
+ * @param peso Contendrá el peso total de los paquetes en kg
+ */
 void enviosDestino(const Contenedor &c, const int destino, int &nro, float &peso);
+
+/**
+ * @brief Analiza y muestra el número de paquetes y el peso total por destino.
+ * @param c Contenedor a analizar.
+ */
 void analisisDestinos(const Contenedor &c);
 
 #endif
--- P1/P1_guion/include/paquete.h
+++ P1/P1_guion/include/paquete.h
@@ -4,23 +4,50 @@
 // Pedro Velasco Santana      //
 ////////////////////////////////
 
-// TO DO: mirar parámetros: valor, referencia, referencia constante?
-
 #ifndef PAQUETE_H
 #define PAQUETE_H
 
 #include <string>
 using namespace std;
 
+/**
+ * @struct Paquete
+ * @brief Representa un paquete con origen, destino y peso en gramos.
+ */
 struct Paquete{
-    int origen;
-    int destino;
-    float peso;
+    int origen;     /**< Origen del paquete */
+    int destino;    /**< Destino del paquete */
+    float peso;     /**< Peso del paquete en gramos */
 };
 
+/**
+ * @brief Convierte gramos a kilogramos. (NO especificada en guion)
+ * @param p Peso en gramos.
+ * @return Peso en kilogramos (float).
+ */
 float gTokg(float p);
+
+/**
+ * @brief Genera una cadena con la información de un paquete.
+ * @param p Paquete a describir.
+ * @return Cadena formateada con la información del paquete.
+ */
 string toString(const Paquete &p);
+
+/**
+ * @brief Crea un paquete a partir de los parámetros especificados.
+ * @param origen Origen del paquete.
+ * @param destino Destino del paquete.
+ * @param peso Peso del paquete en gramos.
+ * @return El paquete creado.
+ */
 Paquete creaPaquete(const int origen, const int destino, const float peso);
+
+/**
+ * @brief Comprueba la validez de un paquete.
+ * @param p Paquete a comprobar.
+ * @return true si es válido, false en caso contrario.
+ */
 bool esValido(const Paquete &p);
 
 #endif
