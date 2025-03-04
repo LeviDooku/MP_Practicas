5df0d5f3da73feed2b9697c4e0e93c70b723a37b
LeviDooku <pedrovelascosantana@gmail.com>
2025-03-04 10:27:52 +0100

    Cambiado comment

--- P2/src/proceso.cpp
+++ P2/src/proceso.cpp
@@ -73,7 +73,7 @@ bool anagrama(const char cad1[],const char cad2[]){
 }
 
 //Se presupone que frase ya está ordenada
-//TODO Está mal
+// ! Está mal
 void compress(const char frase[], char salida []){
     int i = 0;
     int contador = 0;
