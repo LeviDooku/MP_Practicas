5c4a9b84f1afec2a7372cb5ae81f1e9928b5b221
LeviDooku <pedrovelascosantana@gmail.com>
2025-04-30 11:23:13 +0200

    Empezando implementación clase ConjuntoParticulas

--- Proyecto_final/include/ConjuntoParticulas.h
+++ Proyecto_final/include/ConjuntoParticulas.h
@@ -4,10 +4,10 @@
 // Pedro Velasco Santana         //
 ///////////////////////////////////
 
-//TODO Comentar rollo doxygen
-
 #ifndef CONJUNTOPARTICULAS_H
 #define CONJUNTOPARTICULAS_H
+
+#include "Particula.h"
 
 const int TAM_BLOQUE = 3;
 
@@ -23,18 +23,70 @@ private:
 
 public:
     
+    /**
+     * @brief Constructor por defecto. 
+     * @param n Define la cantidad de particulas del conjunto. Se generan de forma aleatoria.
+     */
     ConjuntoParticulas(const int n = 0);
+    
+    /**
+     * @brief Constructor de copia
+     * @param otro Conjunto a copiar
+     */
     ConjuntoParticulas(const ConjuntoParticulas &otro);
+
+    /**
+     * @brief Destructor, libera la memoria reservada
+     */
     ~ConjuntoParticulas();
 
+    /**
+     * @brief Devuelve la cantidad de Partículas en el conjunto
+     * @return Cantidad de partículas del conjunto
+     */
     int getUtiles() const;
     
+    /**
+     * @brief Agrega una partícula al conjunto. Si no hay hueco, redimensiona la capacidad
+     * @param a_agregar Particula a añadir
+     */
     void agrega(const Particula &a_agregar);
+
+    /**
+     * @brief Borra la particula de una posición concreta
+     * @param pos Posición del array en el que se realiza el borrado
+     */
     void borrar(const int pos);
-    Particula& obtener(const int pos); // ? Not sure si esto esta bien
+
+    /**
+     * @brief Devuelve una referencia al elemento de una posición concreta
+     * @param pos Posición del elemento
+     * @return Referencia al elemento pos
+     */
+    Particula& obtener(const int pos); 
+
+    /**
+     * @brief Reemplaza un elemento concreto de un array con otro
+     * @param sustituta Elemento a reemplazar
+     * @param pos Posición en la que se realiza el reemplazo
+     */
     void reemplazar(const Particula &sustituta, const int pos);
-    void mover(int tipo = 0);
-    void gestionarColisones();
+
+    /**
+     * @brief Mueve las Partículas del conjunto en base a un tipo
+     * @param tipo Tipo de movimiento de las partículas
+     */
+    void mover(const int tipo = 0);
+
+    /**
+     * @brief Evalúa si las partículas han chocado
+     */
+    void gestionarColisiones();
+
+    /**
+     * @brief Devuelve los datos del objeto actual como string
+     * @return Cadena con los datos formateados
+     */
     std::string toString() const;
 };
 
--- Proyecto_final/src/ConjuntoParticulas.cpp
+++ Proyecto_final/src/ConjuntoParticulas.cpp
@@ -6,4 +6,60 @@
 
 #include <iostream>
 #include "ConjuntoParticulas.h"
-#include "Particula.h"
+//#include "Particula.h"
+
+ConjuntoParticulas::ConjuntoParticulas(const int n){
+    if(n > 0){
+        set = new Particula[n];
+        capacidad = n;
+        utiles = n;
+
+        for(int i = 0; i < n; ++i)
+            set[i] = Particula(1);
+    }
+}
+
+ConjuntoParticulas::ConjuntoParticulas(const ConjuntoParticulas &otro){ // ? No deberían tmb copiarse la cap??
+    capacidad = otro.capacidad;
+    utiles = otro.utiles;
+    set = new Particula[capacidad];
+
+    for(int i = 0; i < capacidad; ++i)
+        set[i] = otro.set[i];
+}
+
+ConjuntoParticulas::~ConjuntoParticulas(){
+
+}
+
+int ConjuntoParticulas::getUtiles() const{
+    return utiles;
+}
+
+void ConjuntoParticulas::agrega(const Particula &a_agregar){
+
+}
+
+void ConjuntoParticulas::borrar(const int pos){
+
+}
+
+Particula& ConjuntoParticulas::obtener(const int pos){
+    return set[pos]; 
+}
+
+void ConjuntoParticulas::reemplazar(const Particula &sustituta, const int pos){
+
+}
+
+void ConjuntoParticulas::mover(int tipo){
+
+}
+
+void ConjuntoParticulas::gestionarColisiones(){
+
+}
+
+std::string ConjuntoParticulas::toString() const{
+
+}
